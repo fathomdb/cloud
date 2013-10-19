7b268394dd537edcd4adbcaa04641a7c9499a8b4
Justin Santa Barbara <justin@fathomdb.com>
2013-10-19 12:33:31 -0700

    Add GET /flavors call (non detailed version)

--- fathomcloud-compute/src/main/java/io/fathom/cloud/compute/api/os/resources/FlavorsResource.java
+++ fathomcloud-compute/src/main/java/io/fathom/cloud/compute/api/os/resources/FlavorsResource.java
@@ -16,23 +16,45 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import com.google.common.collect.Lists;
 
 @Path("/openstack/compute/{project}/flavors")
+@Produces({ MediaType.APPLICATION_JSON })
 public class FlavorsResource extends FathomCloudResourceBase {
     @Inject
     Flavors flavors;
 
     @GET
+    public ListFlavorsResponse listFlavors() throws CloudException {
+        return listFlavors(false);
+    }
+
+    @GET
     @Path("detail")
-    @Produces({ JSON })
-    public ListFlavorsResponse doFlavorsList() throws CloudException {
+    public ListFlavorsResponse listFlavorsDetailed() throws CloudException {
+        return listFlavors(true);
+    }
+
+    @GET
+    @Path("{id}")
+    public WrappedFlavor showFlavor(@PathParam("id") long id) throws CloudException {
+        FlavorData flavor = flavors.find(id);
+        notFoundIfNull(flavor);
+
+        WrappedFlavor response = new WrappedFlavor();
+        response.flavor = toModel(flavor, true);
+
+        return response;
+    }
+
+    private ListFlavorsResponse listFlavors(boolean details) throws CloudException {
         ListFlavorsResponse response = new ListFlavorsResponse();
         response.flavors = Lists.newArrayList();
 
         for (FlavorData flavor : flavors.list()) {
-            response.flavors.add(toModel(flavor));
+            response.flavors.add(toModel(flavor, details));
         }
 
         Collections.sort(response.flavors, new Comparator<Flavor>() {
@@ -45,20 +67,7 @@ public class FlavorsResource extends FathomCloudResourceBase {
         return response;
     }
 
-    @GET
-    @Path("{id}")
-    @Produces({ JSON })
-    public WrappedFlavor showFlavor(@PathParam("id") long id) throws CloudException {
-        FlavorData flavor = flavors.find(id);
-        notFoundIfNull(flavor);
-
-        WrappedFlavor response = new WrappedFlavor();
-        response.flavor = toModel(flavor);
-
-        return response;
-    }
-
-    Flavor toModel(FlavorData data) {
+    Flavor toModel(FlavorData data, boolean details) {
         Flavor flavor = new Flavor();
 
         flavor.id = data.getId() + "";
