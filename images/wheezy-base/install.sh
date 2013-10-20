20e1bf9505f3cae72d15cb920f011357fc6e24df
Justin Santa Barbara <justin@fathomdb.com>
2013-10-19 19:26:59 -0700

    Use proxies when building; more to script

--- images/wheezy-base/install.sh
+++ images/wheezy-base/install.sh
@@ -3,6 +3,33 @@
 set -e
 set -x
 
+# Make sure we're up to date
+apt-get update
+
+# If we don't install sudo, then anything that needs sudo just fails
+# Ubuntu is very sudo reliant..
+apt-get install --yes --no-install-recommends sudo
+
+# Fix up locales
+apt-get install --yes --no-install-recommends locales
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+echo "LANG=en_US.UTF-8" > /etc/default/locale
+locale-gen en_US.utf8
+DEBIAN_FRONTEND=noninteractive dpkg-reconfigure locales
+
+# Install SSH
+apt-get install --yes --no-install-recommends openssh-server
+
+# For Ubuntu only (?)
+rm -f rootfs/etc/init/plymouth.conf
+
+# Lock down SSH; disable DNS
+sed -i "s/#PasswordAuthentication yes/PasswordAuthentication no/g" /etc/ssh/sshd_config
+/bin/echo -e "\n\nUseDNS no" >> /etc/ssh/sshd_config
+
+# Disable most gettys
+sed -i "s/.:23:respawn:.sbin.getty/#&/g" /etc/inittab
+
 # Create mountpoints for volumes
 mkdir -p /volumes/ephemeral/
 mkdir -p /volumes/persistent/
