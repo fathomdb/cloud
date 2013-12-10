3f1e55f9de79f7012d7c22c32bcf4db24bc52d86
Justin Santa Barbara <justin@fathomdb.com>
2013-12-10 11:21:53 -0500

    Fix typo in keygen script, which meant fathomcloud wasn't self-authorized for SSH

--- autonomous/auto-fathomcloud/image/keygen.sh
+++ autonomous/auto-fathomcloud/image/keygen.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+set -e
+set -x
+
 if [[ ! -f ~fathomcloud/.ssh/id_rsa ]]; then
 	# Generate the ssh key
 	su fathomcloud -c "ssh-keygen -q -t rsa -f ~/.ssh/id_rsa -N \"\"" -s /bin/bash
 	
 	# Make sure the key is authorized
-	cat ~fathomcloud/.ssh/id_rsa.pub | su fathomcloud tee -a ~fathomcloud/.ssh/authorized_keys
+	cat ~fathomcloud/.ssh/id_rsa.pub | su fathomcloud -c "tee -a ~fathomcloud/.ssh/authorized_keys"
 fi
